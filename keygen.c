847c7c9e68f5246fdab19cd8a8cecb938da92fd9
Steven Owens <owenss@oregonstate.edu>
2020-07-25 07:05:20 -0700

    implemented keygen program using mod 27 operations

--- keygen.c
+++ keygen.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 int main(int argc, char *argv[])
 {
+    srand(time(0));
 
-    for (int i = 0; i < argc; i++)
+    if(argc != 2)
     {
-        fprintf(stderr, "%s\n", argv[i]);
+        fprintf(stderr, "correct usage: %s <length>\n", argv[0]);
+        fflush(stderr);
+        return(-1);
     }
+
+    int length = atoi(argv[1]);
+    char key[length + 1];  // length + 1 for null terminator
+
+    char *allowed = "ABCDEFGHIJKLMOPQRSTUVWXYZ ";
     
-    return 0;
+    for(int i = 0; i < length; i++)
+    {
+        key[i] = allowed[rand() % 27];
+    }
+
+    key[length] = '\0';
+
+    fprintf(stdout, "KEY: %s\n", key);
+    fflush(stdout);
+    
+  return (0);
 }
